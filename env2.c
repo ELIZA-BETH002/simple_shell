f2b90c0afbd8a114b0d0ae3269ce62b0a82da619
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-04-26 19:13:52 +0100

    Update env2.c

--- env2.c
+++ env2.c
@@ -19,14 +19,14 @@ char **get_environ(info_t *info)
 }
 
 /**
- * _unsetenv - Remove an environment variable
+ * _unsetenvv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: 1 on delete, 0 otherwise
  * @var: the string env var property
 */
 
-int _unsetenv(info_t *info, char *var)
+int _unsetenvv(info_t *info, char *var)
 {
 	list_t *node = info->env;
 	size_t i = 0;
@@ -52,7 +52,7 @@ int _unsetenv(info_t *info, char *var)
 }
 
 /**
- * _setenv - Initialize a new environment variable,
+ * _setenvv - Initialize a new environment variable,
  *             or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
@@ -61,7 +61,7 @@ int _unsetenv(info_t *info, char *var)
  *  Return: Always 0
 */
 
-int _setenv(info_t *info, char *var, char *value)
+int _setenvv(info_t *info, char *var, char *value)
 {
 	char *buf = NULL;
 	list_t *node;
