bf5a290173924eeaec09ef9050ed3df721947e24
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-04-26 20:39:56 +0100

    Update list_len.c

--- list_len.c
+++ list_len.c
@@ -1,127 +1,36 @@
 #include "main.h"
 
-/**
- * list_len - determines length of linked list
- * @h: pointer to first node
- *
- * Return: size of list
-**/
-
-size_t list_len(const list_t *h)
-{
-	size_t i = 0;
-
-	while (h)
-	{
-		h = h->next;
-		i++;
-	}
-	return (i);
-}
 
 /**
- * list_strings - returns an array of strings of the list->str
- * @head: pointer to first node
- *
- * Return: array of strings
-**/
-
-char **list_strings(list_t *head)
+ * builtins_list - search for match and execute the associate builtin
+ * @data: struct for the program's data
+ * Return: Returns the return of the function executed is there is a match,
+ * otherwise returns -1.
+ **/
+int builtins_list(data_of_program *data)
 {
-	list_t *node = head;
-	size_t i = list_len(head), j;
-	char **strs;
-	char *str;
+	int iterator;
+	builtins options[] = {
+		{"exit", builtin_exit},
+		{"help", builtin_help},
+		{"cd", builtin_cd},
+		{"alias", builtin_alias},
+		{"env", builtin_env},
+		{"setenv", builtin_set_env},
+		{"unsetenv", builtin_unset_env},
+		{NULL, NULL}
+	};
 
-	if (!head || !i)
-		return (NULL);
-	strs = malloc(sizeof(char *) * (i + 1));
-	if (!strs)
-		return (NULL);
-	for (i = 0; node; node = node->next, i++)
+/*walk through the structure*/
+	for (iterator = 0; options[iterator].builtin != NULL; iterator++)
 	{
-		str = malloc(_strlen(node->str) + 1);
-		if (!str)
+/*if there is a match between the given command and a builtin,*/
+		if (str_compare(options[iterator].builtin, data->command_name, 0))
 		{
-			for (j = 0; j < i; j++)
-				free(strs[j]);
-			free(strs);
-			return (NULL);
+/*execute the function, and return the return value of the function*/
+			return (options[iterator].function(data));
 		}
-
-		str = _strcpy(str, node->str);
-		strs[i] = str;
-	}
-	strs[i] = NULL;
-	return (strs);
-}
-
-
-/**
- * print_list - prints all elements of a list_t linked list
- * @h: pointer to first node
- *
- * Return: size of list
-*/
-
-size_t print_list(const list_t *h)
-{
-	size_t i = 0;
-
-	while (h)
-	{
-		_puts(convert_num(h->num, 10, 0));
-		_putchar(':');
-		_putchar(' ');
-		_puts(h->str ? h->str : "(nil)");
-		_puts("\n");
-		h = h->next;
-		i++;
-	}
-	return (i);
-}
-
-/**
- * node_start_with - returns node whose string starts with prefix
- * @node: pointer to list head
- * @prefix: string to match
- * @c: the next character after prefix to match
- *
- * Return: match node or null
-*/
-
-list_t *node_start_with(list_t *node, char *prefix, char c)
-{
-	char *p = NULL;
-
-	while (node)
-	{
-		p = starts_with(node->str, prefix);
-		if (p && ((c == -1) || (*p == c)))
-			return (node);
-		node = node->next;
-	}
-	return (NULL);
-}
-
-/**
- * get_node_index - gets the index of a node
- * @head: pointer to list head
- * @node: pointer to the node
- *
- * Return: index of node or -1
-*/
-
-ssize_t get_node_index(list_t *head, list_t *node)
-{
-	size_t i = 0;
-
-	while (head)
-	{
-		if (head == node)
-			return (i);
-		head = head->next;
-		i++;
+/*if there is no match return -1 */
 	}
 	return (-1);
 }
