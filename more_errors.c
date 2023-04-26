1078ab8996677e824b345335c80aaacd2a30bd88
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-04-26 20:42:20 +0100

    more

--- more_errors.c
+++ more_errors.c
@@ -1,148 +1,159 @@
 #include "main.h"
 
 /**
- * _erratoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
- *       -1 on error
-*/
-
-int _erratoi(char *s)
-{
-	int i = 0;
-	unsigned long int result = 0;
-
-	if (*s == '+')
-		s++;
-	for (i = 0;  s[i] != '\0'; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-			if (result > INT_MAX)
-				return (-1);
-		}
-		else
-			return (-1);
-	}
-	return (result);
-}
-
-/**
- * print_error - prints an error message
- * @info: the parameter & return info struct
- * @estr: string containing specified error type
- * Return: 0 if no numbers in string, converted number otherwise
- *        -1 on error
-*/
-
-void print_error(info_t *info, char *estr)
-{
-	_puts(info->fname);
-	_puts(": ");
-	print_d(info->line_count, STDERR_FILENO);
-	_puts(": ");
-	_puts(info->argv[0]);
-	_puts(": ");
-	_puts(estr);
-}
-
-/**
- * print_d - function prints a decimal (integer) number (base 10)
- * @input: the input
- * @fd: the filedescriptor to write to
- *
- * Return: number of characters printed
-*/
-
-int print_d(int input, int fd)
-{
-	int (*__putchar)(char) = _putchar;
-	int i, count = 0;
-	unsigned int _abs_, current;
-
-	if (fd == STDERR_FILENO)
-		__putchar = _eputchar;
-	if (input < 0)
-	{
-		_abs_ = -input;
-		__putchar('-');
-		count++;
-	}
-	else
-		_abs_ = input;
-	current = _abs_;
-	for (i = 1000000000; i > 1; i /= 10)
-	{
-		if (_abs_ / i)
-		{
-			__putchar('0' + current / i);
-			count++;
-		}
-		current %= i;
-	}
-	__putchar('0' + current);
-	count++;
-
-	return (count);
-}
-
-/**
- * convert_num - converter function, a clone of itoa
- * @num: number
- * @base: base
- * @flags: argument flags
- *
- * Return: string
-*/
-
-char *convert_num(long int num, int base, int flags)
-{
-	static char *array;
-	static char buffer[50];
-	char sign = 0;
-	char *ptr;
-	unsigned long n = num;
-
-	if (!(flags & CONVERT_UNSIGNED) && num < 0)
-	{
-		n = -num;
-		sign = '-';
-
-	}
-
-	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
-	ptr = &buffer[49];
-	*ptr = '\0';
-
-	do	{
-		*--ptr = array[n % base];
-		n /= base;
-	}
-
-    while (n != 0);
-
-	if (sign)
-		*--ptr = sign;
-	return (ptr);
-}
-
-/**
- * remove_comment - function replaces first instance of '#' with '\0'
- * @buf: address of the string to modify
- *
- * Return: Always 0;
-*/
-
-void remove_comment(char *buf)
+ * builtin_exit - exit of the program with the status
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+int builtin_exit(data_of_program *data)
 {
 	int i;
 
-	for (i = 0; buf[i] != '\0'; i++)
-		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+	if (data->tokens[1] != NULL)
+	{/*if exists arg for exit, check if is a number*/
+		for (i = 0; data->tokens[1][i]; i++)
+			if ((data->tokens[1][i] < '0' || data->tokens[1][i] > '9')
+				&& data->tokens[1][i] != '+')
+			{/*if is not a number*/
+				errno = 2;
+				return (2);
+			}
+		errno = _atoi(data->tokens[1]);
+	}
+	free_all_data(data);
+	exit(errno);
+}
+
+/**
+ * builtin_cd - change the current directory
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+int builtin_cd(data_of_program *data)
+{
+	char *dir_home = env_get_key("HOME", data), *dir_old = NULL;
+	char old_dir[128] = {0};
+	int error_code = 0;
+
+	if (data->tokens[1])
+	{
+		if (str_compare(data->tokens[1], "-", 0))
 		{
-			buf[i] = '\0';
-			break;
+			dir_old = env_get_key("OLDPWD", data);
+			if (dir_old)
+				error_code = set_work_directory(data, dir_old);
+			_print(env_get_key("PWD", data));
+			_print("\n");
+
+			return (error_code);
 		}
+		else
+		{
+			return (set_work_directory(data, data->tokens[1]));
+		}
+	}
+	else
+	{
+		if (!dir_home)
+			dir_home = getcwd(old_dir, 128);
+
+		return (set_work_directory(data, dir_home));
+	}
+	return (0);
+}
+
+/**
+ * set_work_directory - set the work directory
+ * @data: struct for the program's data
+ * @new_dir: path to be set as work directory
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+int set_work_directory(data_of_program *data, char *new_dir)
+{
+	char old_dir[128] = {0};
+	int err_code = 0;
+
+	getcwd(old_dir, 128);
+
+	if (!str_compare(old_dir, new_dir, 0))
+	{
+		err_code = chdir(new_dir);
+		if (err_code == -1)
+		{
+			errno = 2;
+			return (3);
+		}
+		env_set_key("PWD", new_dir, data);
+	}
+	env_set_key("OLDPWD", old_dir, data);
+	return (0);
+}
+
+/**
+ * builtin_help - shows the environment where the shell runs
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+int builtin_help(data_of_program *data)
+{
+	int i, length = 0;
+	char *mensajes[6] = {NULL};
+
+	mensajes[0] = HELP_MSG;
+
+	/* validate args */
+	if (data->tokens[1] == NULL)
+	{
+		_print(mensajes[0] + 6);
+		return (1);
+	}
+	if (data->tokens[2] != NULL)
+	{
+		errno = E2BIG;
+		perror(data->command_name);
+		return (5);
+	}
+	mensajes[1] = HELP_EXIT_MSG;
+	mensajes[2] = HELP_ENV_MSG;
+	mensajes[3] = HELP_SETENV_MSG;
+	mensajes[4] = HELP_UNSETENV_MSG;
+	mensajes[5] = HELP_CD_MSG;
+
+	for (i = 0; mensajes[i]; i++)
+	{
+		length = str_length(data->tokens[1]);
+		if (str_compare(data->tokens[1], mensajes[i], length))
+		{
+			_print(mensajes[i] + length + 1);
+			return (1);
+		}
+	}
+	/*if there is no match, print error and return -1 */
+	errno = EINVAL;
+	perror(data->command_name);
+	return (0);
+}
+
+/**
+ * builtin_alias - add, remove or show aliases
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+int builtin_alias(data_of_program *data)
+{
+	int i = 0;
+
+	/* if there are no arguments, print all environment */
+	if (data->tokens[1] == NULL)
+		return (print_alias(data, NULL));
+
+	while (data->tokens[++i])
+	{/* if there are arguments, set or print each env variable*/
+		if (count_characters(data->tokens[i], "="))
+			set_alias(data->tokens[i], data);
+		else
+			print_alias(data, data->tokens[i]);
+	}
+
+	return (0);
 }
