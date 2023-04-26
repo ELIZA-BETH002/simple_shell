0198039e50c33f6c36a79db69a2e6be427461378
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-04-26 20:47:33 +0100

    Update more_func.c

--- more_func.c
+++ more_func.c
@@ -1,76 +1,112 @@
 #include "main.h"
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
+ * expand_variables - expand variables
+ * @data: a pointer to a struct of the program's data
  *
- * Return: 1 if interactive mode, 0 otherwise
-**/
-
-int interactive(info_t *info)
+ * Return: nothing, but sets errno.
+ */
+void expand_variables(data_of_program *data)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
-}
+	int i, j;
+	char line[BUFFER_SIZE] = {0}, expansion[BUFFER_SIZE] = {'\0'}, *temp;
 
-/**
- * _delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
-*/
-
-int _delim(char c, char *delim)
-{
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
-}
-
-/**
- *_alpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
-*/
-
-int _alpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
-
-/**
- *_atoi - converts a string to an integer
- *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
-*/
-
-int _atoi(char *s)
-{
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
-
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
+	if (data->input_line == NULL)
+		return;
+	buffer_add(line, data->input_line);
+	for (i = 0; line[i]; i++)
+		if (line[i] == '#')
+			line[i--] = '\0';
+		else if (line[i] == '$' && line[i + 1] == '?')
 		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
+			line[i] = '\0';
+			long_to_string(errno, expansion, 10);
+			buffer_add(line, expansion);
+			buffer_add(line, data->input_line + i + 2);
 		}
-		else if (flag == 1)
-			flag = 2;
+		else if (line[i] == '$' && line[i + 1] == '$')
+		{
+			line[i] = '\0';
+			long_to_string(getpid(), expansion, 10);
+			buffer_add(line, expansion);
+			buffer_add(line, data->input_line + i + 2);
+		}
+		else if (line[i] == '$' && (line[i + 1] == ' ' || line[i + 1] == '\0'))
+			continue;
+		else if (line[i] == '$')
+		{
+			for (j = 1; line[i + j] && line[i + j] != ' '; j++)
+				expansion[j - 1] = line[i + j];
+			temp = env_get_key(expansion, data);
+			line[i] = '\0', expansion[0] = '\0';
+			buffer_add(expansion, line + i + j);
+			temp ? buffer_add(line, temp) : 1;
+			buffer_add(line, expansion);
+		}
+	if (!str_compare(data->input_line, line, 0))
+	{
+		free(data->input_line);
+		data->input_line = str_duplicate(line);
 	}
+}
 
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
+/**
+ * expand_alias - expans aliases
+ * @data: a pointer to a struct of the program's data
+ *
+ * Return: nothing, but sets errno.
+ */
+void expand_alias(data_of_program *data)
+{
+	int i, j, was_expanded = 0;
+	char line[BUFFER_SIZE] = {0}, expansion[BUFFER_SIZE] = {'\0'}, *temp;
 
-	return (output);
+	if (data->input_line == NULL)
+		return;
+
+	buffer_add(line, data->input_line);
+
+	for (i = 0; line[i]; i++)
+	{
+		for (j = 0; line[i + j] && line[i + j] != ' '; j++)
+			expansion[j] = line[i + j];
+		expansion[j] = '\0';
+
+		temp = get_alias(data, expansion);
+		if (temp)
+		{
+			expansion[0] = '\0';
+			buffer_add(expansion, line + i + j);
+			line[i] = '\0';
+			buffer_add(line, temp);
+			line[str_length(line)] = '\0';
+			buffer_add(line, expansion);
+			was_expanded = 1;
+		}
+		break;
+	}
+	if (was_expanded)
+	{
+		free(data->input_line);
+		data->input_line = str_duplicate(line);
+	}
+}
+
+/**
+ * buffer_add - append string at end of the buffer
+ * @buffer: buffer to be filled
+ * @str_to_add: string to be copied in the buffer
+ * Return: nothing, but sets errno.
+ */
+int buffer_add(char *buffer, char *str_to_add)
+{
+	int length, i;
+
+	length = str_length(buffer);
+	for (i = 0; str_to_add[i]; i++)
+	{
+		buffer[length + i] = str_to_add[i];
+	}
+	buffer[length + i] = '\0';
+	return (length + i);
 }
