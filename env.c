cd0502f3abf57acb7ff84146c7cfdc538f5e232f
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-04-26 19:39:24 +0100

    Update env.c

--- env.c
+++ env.c
@@ -48,7 +48,7 @@ int _setenv(info_t *info)
 {
 	if (info->argc != 3)
 	{
-		_eputs("Incorrect number of arguements\n");
+		_puts("Incorrect number of arguements\n");
 		return (1);
 	}
 	if (_setenv(info, info->argv[1], info->argv[2]))
